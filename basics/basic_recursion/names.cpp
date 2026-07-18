e5f2e12bd5403603fd076a50bedcfaad453a1e69
ahadullabaig <ahadullabaig.16@gmail.com>
2026-07-18 17:38:18 +0530

    solved the first few recursion problems

---
+++ basics/basic_recursion/names.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print(int n)
+{
+    if(n == 0) return;
+
+    cout << "Ahad" << endl;
+
+    print(n-1);
+}
+
+int main()
+{
+    int n = 5;
+
+    print(n);
+
+    return 0;
+}
