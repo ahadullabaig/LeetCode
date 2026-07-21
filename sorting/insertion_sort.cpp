a2b1a7ed5658f5de584366fc679f41cf59fd36be
ahadullabaig <ahadullabaig.16@gmail.com>
2026-07-21 20:21:46 +0530

    solved recursive insertion_sort

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+void recursive_insertion_sort(vector<int> &arr, int n)
+{
+    if(n == arr.size()) return;
+
+    int key = arr[n];
+
+    int j = n-1;
+
+    while(j>=0 && arr[j] > key)
+    {
+        arr[j+1] = arr[j];
+        j--;
+    }
+
+    arr[j+1] = key;
+
+    recursive_insertion_sort(arr, n+1);
+}
+
 void insertion_sort(vector<int> &arr)
 {
     int n = arr.size();
@@ -26,7 +45,7 @@ int main()
 {
     vector<int> arr = {6, 7, 5, 4, 3, 2, 1};
 
-    insertion_sort(arr);
+    recursive_insertion_sort(arr, 1);
 
     for(int x : arr) cout << x << " ";
 
