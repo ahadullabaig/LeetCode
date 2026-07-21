d3fb809ddd639c4cb26519fd86ff271199dedfe2
ahadullabaig <ahadullabaig.16@gmail.com>
2026-07-21 20:21:33 +0530

    solved recursive bubble_sort

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+void recursive_bubble_sort(vector<int> &arr, int n)
+{
+    if(n == 1) return;
+
+    bool swapped = false;
+
+    for(int i=0; i < (n-1); i++)
+    {
+        if(arr[i] > arr[i+1])
+        {
+            swap(arr[i], arr[i+1]);
+            swapped = true;
+        }
+    }
+
+    if(!swapped) return;
+
+    recursive_bubble_sort(arr, n-1);
+}
+
 void bubble_sort(vector<int> &arr)
 {
     int n = arr.size();
@@ -27,7 +47,9 @@ int main()
 {
     vector<int> arr = {5, 4, 3, 2, 1};
 
-    bubble_sort(arr);
+    int n = arr.size();
+
+    recursive_bubble_sort(arr, n);
 
     for(int x : arr) cout << x << " ";
 
