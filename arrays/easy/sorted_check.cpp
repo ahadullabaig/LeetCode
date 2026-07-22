52771a4b7f396d9e7570e23db7dccb3f7dc4297d
ahadullabaig <ahadullabaig.16@gmail.com>
2026-07-22 22:50:39 +0530

    solved Q1752

--- arrays/easy/sorted_check.cpp
+++ arrays/easy/sorted_check.cpp
@@ -14,12 +14,29 @@ bool isSorted(vector<int> &arr)
     return true;
 }
 
+bool leetcode(vector<int> &nums)
+{
+    int n = nums.size();
+
+    int counter = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        if(nums[i] > nums[(i+1) % n])
+        {
+            counter++;
+        }
+    }
+
+    return counter <= 1;
+}
+
 int main()
 {
     vector<int> arr = {1, 2, 4, 5, 6, 7, 9};
 
     if(isSorted(arr)) cout << "Array is Sorted" << endl;
-    else cout << "Array is not Sorted" << endl;;
+    else cout << "Array is not Sorted" << endl;
 
     return 0;
 }
