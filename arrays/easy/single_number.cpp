0956f9648b8326a2ac6783724194a3d9dd82d53c
ahadullabaig <ahadullabaig.16@gmail.com>
2026-07-27 20:33:35 +0530

    solved Q137

--- arrays/easy/single_number.cpp
+++ arrays/easy/single_number.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int single_number(vector<int> &nums)
+int single_number_1(vector<int> &nums)
 {
     int num = 0;
     
@@ -11,11 +11,30 @@ int single_number(vector<int> &nums)
     return num;
 }
 
+int single_number_2(vector<int> &nums)
+{
+    int ans = 0;
+    
+    for(int i=0; i<32; i++)
+    {
+        int count = 0;
+
+        for(int x : nums)
+        {
+            if(x & (1 << i)) count++;
+        }
+
+        if(count % 3 == 1) ans |= (1 << i);
+    }
+
+    return ans;
+}
+
 int main()
 {
-    vector<int> arr = {1, 1, 2, 3, 3};
+    vector<int> arr = {1, 1, 1, 2, 3, 3, 3};
 
-    cout << single_number(arr) << endl;
+    cout << single_number_2(arr) << endl;
 
     return 0;
 }
