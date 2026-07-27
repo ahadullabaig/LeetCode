90726f60a5b02086d7d2d9e65cc6d6d73f736ea2
ahadullabaig <ahadullabaig.16@gmail.com>
2026-07-27 23:23:10 +0530

    solved longest_subarray with (sum == k) for -ve, 0, and +ve numbers

--- arrays/easy/longest_subarray_sum_k.cpp
+++ arrays/easy/longest_subarray_sum_k.cpp
@@ -26,11 +26,41 @@ int longest_subarray(vector<int> &nums, int k)
     return longest;
 }
 
+int longest_subarray_with_negatives(vector<int> &nums, int k)
+{
+    int longest = 0;
+
+    unordered_map<int, int> sums;
+
+    int sum = 0;
+
+    for(int i=0; i<nums.size(); i++)
+    {
+        sum += nums[i];
+
+        if(sum == k) longest = i+1;
+
+        int prefix = sum - k;
+
+        if(sums.find(prefix) != sums.end())
+        {
+            longest = max(longest, i - sums[prefix]);
+        }
+
+        if(sums.find(sum) == sums.end())
+        {
+            sums[sum] = i;
+        }
+    }
+
+    return longest;
+}
+
 int main()
 {
-    vector<int> arr = {5, 2, 3, 1};
+    vector<int> arr = {6, -2, 2, -8, 1, 7, 4, -10};
 
-    cout << longest_subarray(arr, 5) << endl;
+    cout << longest_subarray_with_negatives(arr, 0) << endl;
 
     return 0;
 }
