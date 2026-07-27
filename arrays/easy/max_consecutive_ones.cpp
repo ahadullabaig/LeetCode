54d747ee73d8716e4dfa79362bbdb96ace494b4e
ahadullabaig <ahadullabaig.16@gmail.com>
2026-07-27 20:10:53 +0530

    solved Q485

---
+++ arrays/easy/max_consecutive_ones.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findMaxConsecutiveOnes(vector<int> &nums)
+{
+    int maximum = 0;
+
+    int left=0, right=0;
+
+    int n = nums.size();
+
+    while(left < n && right < n)
+    {
+        if(nums[right] != 1)
+        {
+            if(maximum < (right - left))
+                maximum = right - left;
+
+            right++;
+            left = right;
+        }
+        else
+        {
+            right++;
+        }
+    }
+
+    if(maximum < (right - left))
+        maximum = right - left;
+
+    return maximum;
+}
+
+int main()
+{
+    return 0;
+}
