3533d9f42621cd6ac6c657fc8881d15c59f90f4c
ahadullabaig <ahadullabaig.16@gmail.com>
2026-07-29 22:38:49 +0530

    solved Q1

---
+++ arrays/medium/two_sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> seen;
+
+    seen[nums[0]] = 0;
+
+    for(int i=1; i<nums.size(); i++)
+    {
+        int required = target - nums[i];
+
+        if(seen.contains(required))
+        {
+            return {i, seen[required]};
+        }
+        else
+        {
+            seen[nums[i]] = i;
+        }
+    }
+
+    return {};
+}
+
+int main()
+{
+    vector<int> nums = {7, 1, 2, 3, 4, 5, 6};
+
+    vector<int> indices = twoSum(nums, 11);
+
+    cout << indices[0] << " " << indices[1] << endl;
+    
+    return 0;
+}
