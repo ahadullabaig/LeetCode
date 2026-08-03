a81597d2d0523365edcae63336ec3e2e57b921c9
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-03 19:07:09 +0530

    solved Q75 with the optimal code

--- arrays/medium/sort_colors.cpp
+++ arrays/medium/sort_colors.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void sortColors(vector<int> &nums)
+void my_sortColors(vector<int> &nums) // my version of O(n) and O(1), which is valid
 {
     int slot = 0;
 
@@ -33,11 +33,36 @@ void sortColors(vector<int> &nums)
     }
 }
 
+void dnf_sortColors(vector<int> &nums) // dutch national flag algorithm
+{
+    int n = nums.size();
+
+    int low = 0, mid = 0, high = n-1;
+
+    while(mid <= high)
+    {
+        if(nums[mid] == 0)
+        {
+            swap(nums[low], nums[mid]);
+            low++; mid++;
+        }
+        else if(nums[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
 int main()
 {
     vector<int> nums = {2, 0, 2, 1, 1, 0};
 
-    sortColors(nums);
+    dnf_sortColors(nums);
 
     for(int x : nums) cout << x << " ";
 
