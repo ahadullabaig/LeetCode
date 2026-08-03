c9e974f3ca4c2b680938e1dfce4f4eaafab6c551
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-03 21:05:16 +0530

    solved Q169 with the optimal code

--- arrays/medium/majority_element.cpp
+++ arrays/medium/majority_element.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int majorityElement(vector<int> &nums)
+int my_majorityElement(vector<int> &nums)
 {
     int n = nums.size();
 
@@ -18,11 +18,44 @@ int majorityElement(vector<int> &nums)
     return 0;
 }
 
+int bm_majorityElement(vector<int> &nums) // Boyer–Moore algorithm
+{
+    int n = nums.size();
+
+    int candidate = -1;
+    
+    int count = 0;
+
+    for(int x : nums)
+    {
+        if(count == 0)
+        {
+            candidate = x;
+            count++;
+        }
+        else
+        {
+            if(x == candidate) count++;
+            else count--;
+        }
+    }
+
+    count = 0;
+
+    for(int x : nums)
+        if(x == candidate)
+            count++;
+
+    if(count > n/2) return candidate;
+
+    return -1;
+}
+
 int main()
 {
     vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
 
-    cout << majorityElement(nums) << endl;
+    cout << bm_majorityElement(nums) << endl;
 
     return 0;
 }
