7d3b1decdbe95d0c25da06c068831630d1b1a7de
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-04 00:14:20 +0530

    solved Q53

---
+++ arrays/medium/maximum_subarray.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int n = nums.size();
+
+    int sum = 0, max_sum = INT_MIN;
+
+    for(int x : nums)
+    {
+        sum += x;
+
+        max_sum = max(sum, max_sum);
+
+        if(sum < 0) sum = 0;
+    }
+
+    return max_sum;
+}
+
+int main()
+{
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    cout << maxSubArray(nums) << endl;
+
+    return 0;
+}
