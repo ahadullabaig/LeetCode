e330c4227daa49c9ed54a99599438f98a428d2d4
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-03 18:06:08 +0530

    modified Q1

--- arrays/medium/two_sum.cpp
+++ arrays/medium/two_sum.cpp
@@ -6,9 +6,7 @@ vector<int> twoSum(vector<int> &nums, int target)
 {
     unordered_map<int, int> seen;
 
-    seen[nums[0]] = 0;
-
-    for(int i=1; i<nums.size(); i++)
+    for(int i=0; i<nums.size(); i++)
     {
         int required = target - nums[i];
 
