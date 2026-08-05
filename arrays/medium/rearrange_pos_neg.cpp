1c732363f4612ef5bf4e8c4c88cc6de0289ae5a5
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-05 22:39:57 +0530

    solved Q2149 with the optimal code

--- arrays/medium/rearrange_pos_neg.cpp
+++ arrays/medium/rearrange_pos_neg.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-vector<int> rearrangeArray(vector<int> &nums)
+vector<int> my_rearrangeArray(vector<int> &nums) // O(n)
 {
     vector<int> alternating;
 
@@ -33,11 +33,38 @@ vector<int> rearrangeArray(vector<int> &nums)
     return alternating;
 }
 
+vector<int> op_rearrangeArray(vector<int> &nums) // O(n)
+{
+    int n = nums.size();
+
+    vector<int> alternating(n);
+
+    int pos = 0, neg = 1;
+
+    for(int x : nums)
+    {
+        if(x > 0)
+        {
+            alternating[pos] = x;
+
+            pos += 2;
+        }
+        else
+        {
+            alternating[neg] = x;
+
+            neg += 2;
+        }
+    }
+
+    return alternating;
+}
+
 int main()
 {
     vector<int> nums = {3, 1, -2, -5, 2, -4};
 
-    vector<int> alternating = rearrangeArray(nums);
+    vector<int> alternating = op_rearrangeArray(nums);
 
     for(int x : alternating) cout << x << " ";
 
