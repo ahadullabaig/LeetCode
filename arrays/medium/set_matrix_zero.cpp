95ae8e30d3b599870c18828348890ffa2de1a939
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-07 23:57:14 +0530

    solved Q73 with the optimal code

--- arrays/medium/set_matrix_zero.cpp
+++ arrays/medium/set_matrix_zero.cpp
@@ -71,13 +71,76 @@ void acceptable_setZeroes(vector<vector<int>> &matrix) // uses O(m+n) space
     }
 }
 
+void op_setZeroes(vector<vector<int>> &matrix)
+{
+    int rows = matrix.size(), cols = matrix[0].size();
+
+    bool first_row = false, first_col = false; // to check if first row/column originally contained any zero
+
+    // first row zero check
+    for(int j=0; j<cols; j++)
+    {
+        if(matrix[0][j] == 0)
+        {
+            first_row = true;
+            break;
+        }
+    }
+
+    // first column zero check
+    for(int i=0; i<rows; i++)
+    {
+        if(matrix[i][0] == 0)
+        {
+            first_col = true;
+            break;
+        }
+    }
+
+    // use the first row and first column as marker arrays
+    for(int i=1; i<rows; i++)
+    {
+        for(int j=1; j<cols; j++)
+        {
+            if(matrix[i][j] == 0)
+            {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // the rest is just zeroing
+
+    for(int i=1; i<rows; i++)
+    {
+        for(int j=1; j<cols; j++)
+        {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if(first_row)
+    {
+        for(int j=0; j<cols; j++) matrix[0][j] = 0;
+    }
+
+    if(first_col)
+    {
+        for(int i=0; i<rows; i++) matrix[i][0] = 0;
+    }
+}
+
 int main()
 {
     vector<vector<int>> matrix = {{0, 1, 2, 0},
                                   {3, 4, 5, 2},
                                   {1, 3, 1, 5}};
 
-    acceptable_setZeroes(matrix);
+    op_setZeroes(matrix);
 
     for(int i=0; i<3; i++)
     {
