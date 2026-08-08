44e7d2e01aedf5fd67b297f284f2c6dc7bf81faf
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-08 23:39:07 +0530

    solved Q54 with a better code

--- arrays/medium/spiral_matrix.cpp
+++ arrays/medium/spiral_matrix.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-vector<int> spiralOrder(vector<vector<int>> &matrix)
+// using vector<pair<int, int>> was not required. O(n*n) space complexity. find() was also inefficient.
+vector<int> my_spiralOrder(vector<vector<int>> &matrix)
 {
     int rows = matrix.size(), cols = matrix[0].size();
 
@@ -89,13 +90,97 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
     return spiral;
 }
 
+// using unordered_set<int> makes O(n) space complexity. contains() is efficient.
+vector<int> better_spiralOrder(vector<vector<int>> &matrix)
+{
+    int rows = matrix.size(), cols = matrix[0].size();
+
+    vector<int> spiral;
+
+    unordered_set<int> indices;
+
+    int i = 0, j = 0, count = 0;
+
+    bool increase = true;
+
+    while(count < (rows * cols))
+    {
+        if(increase)
+        {
+            while(j < cols && !indices.contains((i*cols) + j))
+            {
+                spiral.push_back(matrix[i][j]);
+
+                indices.insert((i*cols) + j);
+                
+                j++; count++;
+            }
+
+            j--; i++;
+
+            if(indices.contains((i*cols) + j))
+            {
+                increase = false;
+                continue;
+            }
+
+            while(i < rows && !indices.contains((i*cols) + j))
+            {
+                spiral.push_back(matrix[i][j]);
+
+                indices.insert((i*cols) + j);
+
+                i++; count++;
+            }
+
+            i--; j--;
+
+            increase = false;
+        }
+        else
+        {
+            while(j >= 0 && !indices.contains((i*cols) + j))
+            {
+                spiral.push_back(matrix[i][j]);
+
+                indices.insert((i*cols) + j);
+
+                j--; count++;
+            }
+
+            j++; i--;
+
+            if(indices.contains((i*cols) + j))
+            {
+                increase = true;
+                continue;
+            }
+
+            while(i >= 0 && !indices.contains((i*cols) + j))
+            {
+                spiral.push_back(matrix[i][j]);
+
+                indices.insert((i*cols) + j);
+
+                i--; count++;
+            }
+
+            i++; j++;
+
+            increase = true;
+        }
+    }
+
+    return spiral;
+}
+
 int main()
 {
     vector<vector<int>> matrix = {{1, 2, 3, 4},
                                   {10, 11, 12, 5},
                                   {9, 8, 7, 6}};
                       
-    vector<int> spiral = spiralOrder(matrix);
+    vector<int> spiral = better_spiralOrder(matrix);
 
     for(int x : spiral) cout << x << " ";
 
