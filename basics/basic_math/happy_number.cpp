5faaffb6918ab65f08c33b850a7e532f88b1b92c
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-14 22:47:09 +0530

    solved Q202 with the optimal code

--- basics/basic_math/happy_number.cpp
+++ basics/basic_math/happy_number.cpp
@@ -27,11 +27,41 @@ bool isHappy(int n)
     return true;
 }
 
+int sum_of_squares(int n)
+{
+    int sum = 0;
+
+    while(n > 0)
+    {
+        sum += pow(n % 10, 2);
+
+        n /= 10;
+    }
+
+    return sum;
+}
+
+bool op_isHappy(int n) // O(1) space complexity. Floyd's Cycle Detection Algorithm.
+{
+    int slow = n;
+
+    int fast = sum_of_squares(n);
+
+    while(fast != 1 && fast != slow)
+    {
+        slow = sum_of_squares(slow);
+
+        fast = sum_of_squares(sum_of_squares(fast)); // always 2 steps ahead
+    }
+
+    return fast == 1;
+}
+
 int main()
 {
     int n = 19;
 
-    if(isHappy(n)) cout << "Happy Number" << endl;
+    if(op_isHappy(n)) cout << "Happy Number" << endl;
 
     else cout << "Not a Happy Number" << endl;
 
