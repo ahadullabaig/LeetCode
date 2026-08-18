9f632df01ee5f77abb861b39da65498f43d719cf
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-18 22:19:15 +0530

    solved the daily for 18-08-2026 with the optimal code

--- daily/q_3471.cpp
+++ daily/q_3471.cpp
@@ -41,13 +41,58 @@ int largestInteger(vector<int> &nums, int k)
     return (largest != INT_MIN) ? largest : -1;
 }
 
+int op_largestInteger(vector<int> &nums, int k) // O(n)
+{
+    int n = nums.size();
+
+    int largest = INT_MIN;
+
+    if(k > 1 && k < n)
+    {
+        if(nums[0] == nums[n-1]) return -1;
+
+        bool left = true, right = true;
+
+        for(int i=1; i < n-1; i++)
+        {
+            if(nums[i] == nums[0]) left = false;
+
+            if(nums[i] == nums[n-1]) right = false;
+        }
+
+        if(left && right) largest = max(nums[0], nums[n-1]);
+
+        else if(left) largest = nums[0];
+
+        else if(right) largest = nums[n-1];
+    }
+    else
+    {
+        unordered_map<int, int> counts;
+
+        for(int x : nums) counts[x]++;
+
+        for(auto [num, count] : counts)
+        {
+            if(count == 1 || k == n)
+            {
+                largest = max(largest, num);
+            }
+        }
+    }
+
+    return (largest != INT_MIN) ? largest : -1;
+}
+
 int main()
 {
-    vector<int> nums = {3, 9, 7, 2, 1, 7};
+    vector<int> nums = {3, 9, 7, 10, 1, 7};
 
     int k = 4;
 
     cout << largestInteger(nums, k) << endl;
+
+    cout << op_largestInteger(nums, k) << endl;
 
     return 0;
 }
