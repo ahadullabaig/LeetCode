b9e0bc44ff278a49434a6724d4cc8a967e37c85a
ahadullabaig <ahadullabaig.16@gmail.com>
2026-08-23 23:23:43 +0530

    solved Q493 with the optimal code

--- arrays/hard/reverse_pairs.cpp
+++ arrays/hard/reverse_pairs.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int reversePairs(vector<int> &nums)
+int bf_reversePairs(vector<int> &nums) // O(n*n)
 {
     int n = nums.size();
 
@@ -21,11 +21,94 @@ int reversePairs(vector<int> &nums)
     return count;
 }
 
+int mergeCount(vector<int> &nums, int left, int mid, int right)
+{
+    vector<int> temp;
+
+    int l = left, r = mid+1;
+
+    while(l <= mid && r <= right)
+    {
+        if(nums[l] <= nums[r])
+        {
+            temp.push_back(nums[l]);
+            l++;
+        }
+        else
+        {
+            temp.push_back(nums[r]);
+            r++;
+        }
+    }
+
+    while(l <= mid)
+    {
+        temp.push_back(nums[l]);
+        l++;
+    }
+
+    while(r <= right)
+    {
+        temp.push_back(nums[r]);
+        r++;
+    }
+
+    int count = 0;
+
+    l = left, r = mid+1;
+
+    while(l <= mid && r <= right)
+    {
+        long long prod = 1LL * nums[r] * 2;
+
+        if(nums[l] > prod)
+        {
+            count += mid - l + 1;
+            
+            r++;
+        }
+
+        else l++;
+    }
+
+    for(int i = left; i <= right; i++)
+    {
+        nums[i] = temp[i - left];
+    }
+
+    return count;
+}
+
+int mergeSort(vector<int> &nums, int left, int right)
+{
+    int count = 0;
+
+    if(left < right)
+    {
+        int mid = (left + right) / 2;
+
+        count += mergeSort(nums, left, mid);
+
+        count += mergeSort(nums, mid+1, right);
+
+        count += mergeCount(nums, left, mid, right);
+    }
+
+    return count;
+}
+
+int op_reversePairs(vector<int> &nums) // O(n*log(n))
+{
+    return mergeSort(nums, 0, nums.size()-1);
+}
+
 int main()
 {
     vector<int> nums = {2, 4, 3, 5, 1};
 
-    cout << reversePairs(nums) << endl;
+    cout << bf_reversePairs(nums) << endl;
+
+    cout << op_reversePairs(nums) << endl;
 
     return 0;
 }
