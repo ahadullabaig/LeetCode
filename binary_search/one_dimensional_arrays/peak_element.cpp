edec2a352095d56bd30f181f778a532be6b81fd1
ahadullabaig <ahadullabaig.16@gmail.com>
2026-09-03 22:09:57 +0530

    solved Q162

---
+++ binary_search/one_dimensional_arrays/peak_element.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findPeakElement(vector<int> &nums)
+{
+    int left = 0, right = nums.size() - 1;
+
+    while(left < right)
+    {
+        int mid = (left + right) / 2;
+
+        if(nums[mid] < nums[mid + 1]) left = mid+1;
+
+        else right = mid;
+    }
+
+    return left;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 1, 3, 5, 6, 4};
+
+    cout << findPeakElement(nums) << endl;
+
+    return 0;
+}
