30d605ecb0ccf29df54b328a10443c827793eac8
ahadullabaig <ahadullabaig.16@gmail.com>
2026-09-07 23:51:52 +0530

    solved the daily for 04-09-2026

--- daily/q_3903.cpp
+++ daily/q_3903.cpp
@@ -46,6 +46,39 @@ int firstStableIndex(vector<int> &nums, int k)
     return -1;
 }
 
+int better_firstStableIndex(vector<int> &nums, int k)
+{
+    int n = nums.size();
+
+    unordered_map<int, int> minimums;
+
+    int minimum = INT_MAX;
+
+    for(int i = n-1; i >= 0; i--)
+    {
+        if(nums[i] < minimum)
+        {
+            minimums[i] = nums[i];
+            minimum = nums[i];
+        }
+
+        else minimums[i] = minimum;
+    }
+
+    int maximum = nums[0];
+
+    for(int i=0; i<n; i++)
+    {
+        maximum = max(maximum, nums[i]);
+
+        int diff = maximum - minimums[i];
+
+        if(diff <= k) return i;
+    }
+
+    return -1;
+}
+
 int main()
 {
     vector<int> nums = {6, 1, 4};
@@ -53,6 +86,8 @@ int main()
     int k = 5;
 
     cout << firstStableIndex(nums, k) << endl;
+
+    cout << better_firstStableIndex(nums, k) << endl;
 
     return 0;
 }
